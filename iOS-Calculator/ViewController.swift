cb5551376b1448258af353f3bf2c1e4147cedf1b
Valerio <valerio.colantonio@sngular.com>
2021-07-21 13:17:06 +0200

    Initial Commit

---
+++ iOS-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Calculator
+//
+//  Created by Valerio Colantonio on 21/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
